951b1d286cbb7e081af2df642896f24c5ada3436
Catalin Palade <catalin@recruitd.com>
2019-11-24 12:43:54 +0000

    Initial Commit

---
+++ poq_CatalinPalade_Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  poq_CatalinPalade_Challenge
+//
+//  Created by Catalin Palade on 24/11/2019.
+//  Copyright © 2019 Catalin Palade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
